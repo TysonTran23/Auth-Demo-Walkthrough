9534eea56737bd51de2e1c4a014d3f20ee5cf3b8
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-07-11 11:52:48 -0700

    first commit

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    username = db.Column(db.Text, nullable=False, unqiue=True)
+
+    password = db.Column(db.Text, nullable=False)
