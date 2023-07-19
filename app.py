94a140945686dfc00c65b7ab96dcfe2c9dab6ed4
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-07-19 12:41:36 -0700

    commit

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, flash, redirect, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
-# from flask_bcrypt import Bcrypt
-
+from forms import UserForm
 from models import User, connect_db, db
-
-# from forms import UserForm, TweetForm
-# from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 # bcrypt = Bcrypt(app)
@@ -18,7 +14,7 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 
 connect_db(app)
-# db.create_all()
+db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
@@ -26,3 +22,39 @@ toolbar = DebugToolbarExtension(app)
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.username.data
+        new_user = User.register(username, password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Welcome! You have successfully created your account")
+        return redirect('/tweets')
+
+    return render_template("register.html", form=form)
+
+@app.route('/tweets')
+def show_tweets():
+    return render_template('tweets.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login_user():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            return redirect('/tweets')
+        else:
+            form.username.errors = ['Invalid Username/Password']
+
+    return render_template('login.html', form=form)
